bae0682fadfd37eb8bd941028af98688594f5913
FAROFAKIDS <farofakids@bol.com.br>
2016-03-29 07:47:08 -0300

    29032016
    29032016

--- Farofakids-Activator/Farofakids-Activator/Program.cs
+++ Farofakids-Activator/Farofakids-Activator/Program.cs
@@ -4,6 +4,7 @@ using EloBuddy.SDK.Menu;
 using EloBuddy;
 using EloBuddy.SDK.Menu.Values;
 using System.Linq;
+using System;
 
 namespace Farofakids_Activator
 {
@@ -12,7 +13,7 @@ namespace Farofakids_Activator
         public static Menu MENU;
 
         public static Spell.Targeted Ignite;
-        public static Spell.Active Heal;
+        public static Spell.Active Heal, Cleanse;
 
         public static readonly Item Zhonyas = new Item(ItemId.Zhonyas_Hourglass);
         public static readonly Item Cutlass = new Item(ItemId.Bilgewater_Cutlass);
@@ -42,6 +43,11 @@ namespace Farofakids_Activator
             get { return MENU["useHeal"].Cast<CheckBox>().CurrentValue; }
         }
 
+        public static bool UseCleanse
+        {
+            get { return MENU["useCleanse"].Cast<CheckBox>().CurrentValue; }
+        }
+
         public static int UseZhonyaheal
         {
             get { return MENU["useZhonyaheal"].Cast<Slider>().CurrentValue; }
@@ -50,6 +56,16 @@ namespace Farofakids_Activator
         public static bool UseZhonyas
         {
             get { return MENU["useZhonyas"].Cast<CheckBox>().CurrentValue; }
+        }
+
+        public static bool UseQss
+        {
+            get { return MENU["useQss"].Cast<CheckBox>().CurrentValue; }
+        }
+
+        public static bool UseTiamat
+        {
+            get { return MENU["useTiamat"].Cast<CheckBox>().CurrentValue; }
         }
 
         public static bool UseCutlass
@@ -84,7 +100,7 @@ namespace Farofakids_Activator
 
         #endregion bool & int MENUVALLUES
 
-        private static void Loading_OnLoadingComplete(System.EventArgs args)
+        private static void Loading_OnLoadingComplete(EventArgs args)
         {
             MENU = MainMenu.AddMenu("Farofakids-Activator", "Farofakids-Activator");
 
@@ -96,29 +112,40 @@ namespace Farofakids_Activator
                 Chat.Print("Farofakids-Activator: Ignite Loaded", System.Drawing.Color.Red);
                 MENU.AddSeparator();
             }
-            if (HasSpell("summonerheal"))
+            if (HasSpell("SummonerHeal"))
             {
                 MENU.AddLabel("Heal");
                 MENU.Add("useHeal", new CheckBox("Use Heal"));
                 MENU.Add("useHealheal", new Slider("Heal at Health (%)", 15));
-                Heal = new Spell.Active(ObjectManager.Player.GetSpellSlotFromName("summonerheal"), 850);
+                Heal = new Spell.Active(ObjectManager.Player.GetSpellSlotFromName("SummonerHeal"), 850);
                 Chat.Print("Farofakids-Activator: Heal Loaded", System.Drawing.Color.Green);
+                MENU.AddSeparator();
+            }
+            if (HasSpell("SummonerBoost"))
+            {
+                MENU.AddLabel("Cleanse");
+                MENU.Add("useCleanse", new CheckBox("Use Cleanse"));
+                Cleanse = new Spell.Active(ObjectManager.Player.GetSpellSlotFromName("SummonerBoost"), 0);
+                Chat.Print("Farofakids-Activator: Cleanse Loaded", System.Drawing.Color.Green);
                 MENU.AddSeparator();
             }
             MENU.AddLabel("ITENS");
             MENU.Add("useZhonyas", new CheckBox("Use Zhonyas"));
             MENU.Add("useZhonyaheal", new Slider("Zhonya at Health (%)", 30));
-            MENU.Add("useYoumuu", new CheckBox("Use Youmuu's Ghostblade"));
-            MENU.Add("useCutlass", new CheckBox("Use Botrk"));
+            MENU.Add("useQss", new CheckBox("Use Quicksilver or Mercurial Scimitar"));
             MENU.Add("useBotrk", new CheckBox("Use Botrk"));
             MENU.Add("minHPBotrk", new Slider("Min health to use Botrk ({0}%)", 80));
             MENU.Add("enemyMinHPBotrk", new Slider("Min enemy health to use Botrk ({0}%)", 80));
+            MENU.Add("useYoumuu", new CheckBox("Use Youmuu's Ghostblade"));
+            MENU.Add("useCutlass", new CheckBox("Use Botrk"));
+            MENU.Add("useTiamat", new CheckBox("Use Tiamat or Hydra or Titanic"));
             Chat.Print("Farofakids-Activator: ITENS Loaded", System.Drawing.Color.HotPink);
 
             Game.OnUpdate += Game_OnUpdate;
+            Orbwalker.OnPostAttack += OnPostAttack;
         }
 
-        private static void Game_OnUpdate(System.EventArgs args)
+        private static void Game_OnUpdate(EventArgs args)
         {
             Execute();
         }
@@ -191,15 +218,58 @@ namespace Farofakids_Activator
 
             #endregion Zhonyas
 
+            #region cleanse
+
+            if (Cleanse != null)
+            {
+                if (UseCleanse && Cleanse.IsReady())
+                {
+                    if (Player.Instance.IsDead || Player.Instance.IsInvulnerable || !Player.Instance.IsTargetable || Player.Instance.IsZombie || Player.Instance.IsInShopRange())
+                        return;
+                    if (Player.HasBuff("PoppyDiplomaticImmunity") || Player.HasBuff("MordekaiserChildrenOfTheGrave") || Player.HasBuff("FizzMarinerDoom") || Player.HasBuff("VladimirHemoplague") ||
+                            Player.HasBuff("zedulttargetmark") || Player.HasBuff("AlZaharNetherGrasp") || Player.HasBuffOfType(BuffType.Suppression) || Player.HasBuffOfType(BuffType.Charm) || Player.HasBuffOfType(BuffType.Flee) || Player.HasBuffOfType(BuffType.Blind) ||
+                            Player.HasBuffOfType(BuffType.Polymorph) || Player.HasBuffOfType(BuffType.Snare) || Player.HasBuffOfType(BuffType.Stun) || Player.HasBuffOfType(BuffType.Taunt))
+                    {
+                        Core.DelayAction(() => Cleanse.Cast(), 110);
+                    }
+
+                }
+            }
+
+            #endregion cleanse
+
+            #region Qss
+
+            if (UseQss)
+            {
+                if (Player.Instance.IsDead || Player.Instance.IsInvulnerable || !Player.Instance.IsTargetable || Player.Instance.IsZombie || Player.Instance.IsInShopRange())
+                    return;
+                InventorySlot[] inv = Player.Instance.InventoryItems;
+                foreach (var item in inv)
+                {
+                    if ((item.Id == ItemId.Quicksilver_Sash || item.Id == ItemId.Mercurial_Scimitar) && item.CanUseItem() && Player.Instance.CountEnemiesInRange(700) > 0)
+                    {
+                        if (Player.HasBuff("PoppyDiplomaticImmunity") || Player.HasBuff("MordekaiserChildrenOfTheGrave") || Player.HasBuff("FizzMarinerDoom") || Player.HasBuff("VladimirHemoplague") ||
+                            Player.HasBuff("zedulttargetmark") || Player.HasBuff("AlZaharNetherGrasp") || Player.HasBuffOfType(BuffType.Suppression) || Player.HasBuffOfType(BuffType.Charm) || Player.HasBuffOfType(BuffType.Flee) || Player.HasBuffOfType(BuffType.Blind) ||
+                            Player.HasBuffOfType(BuffType.Polymorph) || Player.HasBuffOfType(BuffType.Snare) || Player.HasBuffOfType(BuffType.Stun) || Player.HasBuffOfType(BuffType.Taunt))
+                        {
+                            Core.DelayAction(() => item.Cast(), 110);
+                        }
+                    }
+                }
+            }
+
+            #endregion Qss
+
             #region Yomuu & Bortk
 
             var target = TargetSelector.GetTarget(550, DamageType.Physical); // 550 = Botrk.Range
-            if (UseYoumuu && Youmuu.IsOwned() && Youmuu.IsReady())
-            {
-                Youmuu.Cast();
-            }
             if (target != null)
             {
+                if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Combo) && UseYoumuu && Youmuu.IsOwned() && Youmuu.IsReady())
+                {
+                    Youmuu.Cast();
+                }
                 if (UseBotrk && Item.HasItem(Cutlass.Id) && Item.CanUseItem(Cutlass.Id) &&
                     Player.Instance.HealthPercent < MinHPBotrk &&
                     target.HealthPercent < EnemyMinHPBotrk)
@@ -215,6 +285,22 @@ namespace Farofakids_Activator
             }
 
             #endregion Yomuu & Bortk
+        }
+
+        private static void OnPostAttack(AttackableUnit target, EventArgs args)
+        {
+            if (!target.IsEnemy || target.IsZombie) return;
+            if (UseTiamat)
+            {
+                InventorySlot[] inv = Player.Instance.InventoryItems;
+                foreach (var item in inv)
+                {
+                    if ((item.Id == ItemId.Tiamat_Melee_Only || item.Id == ItemId.Ravenous_Hydra_Melee_Only || item.Id == ItemId.Titanic_Hydra) && item.CanUseItem() && Player.Instance.CountEnemiesInRange(400) > 0)
+                    {
+                        item.Cast();
+                    }
+                }
+            }
         }
     }
 }
