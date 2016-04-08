8679f845aa54fcc31a05870af0213f6517c605e7
FAROFAKIDS <farofakids@bol.com.br>
2016-04-08 12:54:43 -0300

    08042016

---
+++ Farofakids-Aurelion Sol/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ElAurelion_Sol
+{
+    using EloBuddy.SDK.Events;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += AurelionSol.OnGameLoad;
+        }
+    }
+}
